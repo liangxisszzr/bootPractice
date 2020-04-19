24a28187c5e63a0ef4c46c00200da748dff751dc
liangxisszzr <49202439+liangxisszzr@users.noreply.github.com>
2020-04-19 18:06:17 +0800

    property verify;userFace upload;exception

--- practice_pojo/src/main/java/com/eastnorth/pojo/bo/center/CenterUserBO.java
+++ practice_pojo/src/main/java/com/eastnorth/pojo/bo/center/CenterUserBO.java
@@ -1,4 +1,4 @@
-package com.imooc.pojo.bo.center;
+package com.eastnorth.pojo.bo.center;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
--- practice_service/src/main/java/com/eastnorth/service/center/CenterUserService.java
+++ practice_service/src/main/java/com/eastnorth/service/center/CenterUserService.java
@@ -1,4 +1,29 @@
 package com.eastnorth.service.center;
 
+import com.eastnorth.pojo.Users;
+import com.eastnorth.pojo.bo.center.CenterUserBO;
+
 public interface CenterUserService {
+
+    /**
+     * 根据用户id查询用户信息
+     * @param userId 用户id
+     * @return 用户信息
+     */
+    Users queryUserInfo(String userId);
+
+    /**
+     * 修改用户信息
+     * @param userId 用户id
+     * @param centerUserBO 用户提交信息
+     */
+    Users updateUserInfo(String userId, CenterUserBO centerUserBO);
+
+    /**
+     * 用户头像更新
+     * @param userId 用户id
+     * @param faceUrl 头像上传位置
+     * @return 用户信息
+     */
+    Users updateUserFace(String userId, String faceUrl);
 }
