aee3071174d391d481b7145404d3bfc71c8a16d9
zuojianhou <zuojianhou@inspur.com>
2020-04-30 15:46:03 +0800

    update

--- practice_api/src/main/java/com/eastnorth/WarStarterApplication.java
+++ practice_api/src/main/java/com/eastnorth/WarStarterApplication.java
@@ -1,6 +1,5 @@
 package com.eastnorth;
 
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
